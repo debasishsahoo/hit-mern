5165ea5b124040eb0ed61f71e36090c2fd686d17
debasish <debasish.sahoo96@gmail.com>
2025-02-07 10:28:05 +0530

    new update

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -15,8 +15,19 @@ const productCtrl = {
     try {
       const products = await productModel.find({});
       res.status(200).json(products);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  },
+  ViewProductById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const product = await productModel.findById({_id:id});
+      if (!product)
+        return res.status(404).json({ message: "product not found" });
+      res.status(200).json(product);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
   },
   UpdateProduct: async (req, res) => {
@@ -41,8 +52,8 @@ const productCtrl = {
       const product = await productModel.findOneAndDelete({_id:id});
       if (!product) return res.status(404).json({ message: 'Product Not Found' });
       res.status(200).json({ message: 'Product Deleted' });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
   },
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,6 +3,8 @@ const productCtrl=require('../controllers/product.controller')
 
 Router.post('/add',productCtrl.AddProduct);
 Router.get('/view',productCtrl.ViewProduct);
+Router.get('/view/:id',productCtrl.ViewProductById);
 Router.put('/update/:id',productCtrl.UpdateProduct);
+Router.delete('/delete/:id',productCtrl.DeleteProduct);
 //Delete Router need to done
 module.exports=Router;
