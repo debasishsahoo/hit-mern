716118ae74df5ce42a745d8ad68c2328a2107e0b
debasish <debasish.sahoo96@gmail.com>
2025-02-13 10:47:56 +0530

    New Update

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -1,12 +1,28 @@
-
-
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import SignIn from './pages/User/SignIn'
+import SignUp from './pages/User/SignUp'
+import ChangePassword from './pages/User/ChangePassword'
+import SignOut from './pages/User/SignOut'
+import GetUser from './pages/User/GetUser'
+import "./App.css";
 
 function App() {
   return (
     <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={ <SignIn/> }/>
+          <Route path="/signin" element={ <SignIn/> }/>
+          <Route path="/signup" element={ <SignUp/> }/>
+          <Route path="/signout" element={ <SignOut/> }/>
+          <Route path="/changepassword" element={ <ChangePassword/> }/>
+          <Route path="/user" element={ <GetUser/> }/>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend2/src/utils/setAuthToken.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const setAuthToken = () => {
+  return (
+    <div>setAuthToken</div>
+  )
+}
+
+export default setAuthToken
