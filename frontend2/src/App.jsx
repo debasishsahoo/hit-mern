144e2078bd7fa33f0a5e785bb4868b9b64e029de
debasish <debasish.sahoo96@gmail.com>
2025-02-13 10:52:31 +0530

    Router Update

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -5,6 +5,10 @@ import SignUp from './pages/User/SignUp'
 import ChangePassword from './pages/User/ChangePassword'
 import SignOut from './pages/User/SignOut'
 import GetUser from './pages/User/GetUser'
+import AddProduct from './pages/Product/AddProduct'
+import ProductList from './pages/Product/ProductList'
+import SingleProduct from './pages/Product/SingleProduct'
+import UpdateProduct from './pages/Product/UpdateProduct'
 import "./App.css";
 
 function App() {
@@ -19,6 +23,10 @@ function App() {
           <Route path="/signout" element={ <SignOut/> }/>
           <Route path="/changepassword" element={ <ChangePassword/> }/>
           <Route path="/user" element={ <GetUser/> }/>
+          <Route path="/product/view" element={ <ProductList/> }/>
+          <Route path="/product/add" element={ <AddProduct/> }/>
+          <Route path="/product/view/:id" element={ <SingleProduct/> }/>
+          <Route path="/product/update/:id" element={ <UpdateProduct/> }/>
         </Routes>
       </Router>
     </>
