60b6bf936ab93469d1ad4d54d3a23ae86ea17cdb
debasish <debasish.sahoo96@gmail.com>
2025-02-13 11:29:30 +0530

    update token

--- frontend2/src/utils/setAuthToken.jsx
+++ frontend2/src/utils/setAuthToken.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import axios from 'axios';
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
 
-const setAuthToken = () => {
-  return (
-    <div>setAuthToken</div>
-  )
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    localStorage.setItem('token', token);
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+    localStorage.removeItem('token');
+  }
 }
 
 export default setAuthToken
