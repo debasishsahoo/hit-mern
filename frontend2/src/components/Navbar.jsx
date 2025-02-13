820bda07143fea5ad0250b7f82c20ccfa33fad75
debasish <debasish.sahoo96@gmail.com>
2025-02-13 12:26:38 +0530

    NavBar

--- frontend2/src/components/Navbar.jsx
+++ frontend2/src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/UserContext";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const { user } = useContext(AuthContext);
 
-export default Navbar
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">ProductApp</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            {user ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/product/view">Products</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signout">Logout</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signin">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
